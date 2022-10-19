3ea68c2b4beeeedc4ba9e2b7d3911c61bdc7324a
unknown <beratcan_acil@hotmail.se>
2022-10-19 23:05:35 +0200

    release

--- gh-build.sh
+++ gh-build.sh
@@ -1,5 +1,11 @@
-#ng build --prod --output-path docs --base-href ./
+ng build --prod --output-path docs --base-href ./
 cd docs
 cp index.html 404.html
 echo ‘nyasidan.se’ >CNAME
+
+# Release
+cd ..
+git add .
+git commit -m "release"
+git push
 read
