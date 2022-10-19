7ea21a8d8c8d18df83b4965a115fb1d82548e5db
unknown <beratcan_acil@hotmail.se>
2022-10-19 22:10:51 +0200

    404 test not work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,8 +19,9 @@ const routes: Routes = [
 
 ];
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes, { useHash: true })],
+  // Tested but did not work
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
