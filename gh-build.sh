45ab3384cd71b74c70e0aa527c19a9ca7adbab93
unknown <beratcan_acil@hotmail.se>
2022-10-19 23:07:20 +0200

    release

--- gh-build.sh
+++ gh-build.sh
@@ -3,9 +3,14 @@ cd docs
 cp index.html 404.html
 echo ‘nyasidan.se’ >CNAME
 
+echo "Docs ready press Enter to releas" 
+read
+
 # Release
 cd ..
 git add .
 git commit -m "release"
 git push
+
+echo "Released..." 
 read
