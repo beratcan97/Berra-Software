a7c71e39e14dbb6e60e21c641ef6aece69cfb9dc
unknown <beratcan_acil@hotmail.se>
2023-01-04 13:08:31 +0100

    version
    s Please enter the commit message for your changes. Lines starting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'berraSoftware';
   version: string = '2.1.1.1.1V';
+
+  ngOnInit(): void {
+    console.log("current version: " + this.version);
+  }
 }
-
-
