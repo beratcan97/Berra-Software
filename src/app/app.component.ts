8554a63e86d1fe44bbe2abfa99bbc0d2228eb4a7
unknown <beratcan_acil@hotmail.se>
2023-01-04 13:11:13 +0100

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'berraSoftware';
-  version: string = '2.1.1.1.1V';
+  version: string = '2.1.1.1.1.1V';
 
   ngOnInit(): void {
     console.log("current version: " + this.version);
